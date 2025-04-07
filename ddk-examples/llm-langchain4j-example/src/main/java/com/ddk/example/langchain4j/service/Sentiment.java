cc3e88d24fd8d205e02e0355d6b1a4c6f73ad46f
Elijah Du <poppycoder@gmail.com>
2025-04-07 21:58:03 +0800

    feat: move langchain4j to another repo.

--- ddk-examples/llm-langchain4j-example/src/main/java/com/ddk/example/langchain4j/service/Sentiment.java
+++
@@ -1,9 +0,0 @@
-package com.ddk.example.langchain4j.service;
-
-/**
- * @author Elijah Du
- * @date 2025/3/16
- */
-public enum Sentiment {
-    POSITIVE, NEUTRAL, NEGATIVE
-}
--- ddk-examples/llm-langchain4j-example/src/test/java/com/ddk/example/langchain4j/Langchain4JApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.ddk.example.langchain4j;
-
-/**
- * @author Elijah Du
- * @date 2025/3/20
- */
-class Langchain4JApplicationTests {
-}
